709c45c2b479fd4c44608ba4aed7a35fd4c8d25c
Aom <oommankung@gmail.com>
2023-03-31 15:46:15 +0700

    add appintment API to swagger UI

--- routes/appointments.js
+++ routes/appointments.js
@@ -70,3 +70,166 @@ module.exports = router;
  *      dentist: 6422f13987d1987cf5239242
  *      createAt: 2023-03-29T07:01:07.968+00:00
  */
+
+/**
+ * @swagger
+ * /appointments:
+ *   get:
+ *    summary: Returns the list of all the appointments
+ *    tags: [Appointments]
+ *    securityDefinitions:
+ *     BearerAuth:
+ *      type: apiKey
+ *      name: Authorization
+ *      in: header
+ *    security:
+ *     - BearerAuth: []
+ *    responses:
+ *     200:
+ *      description: The list of the appointments
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: array
+ *         items:
+ *          $ref: '#/components/schemas/Appointment'
+ *     500:
+ *      description: Can't find appointment
+ */
+
+/**
+ * @swagger
+ * /appointments/{id}:
+ *  get:
+ *   summary: Return an appointment which match ID
+ *   tags: [Appointments]
+ *   securityDefinitions:
+ *    BearerAuth:
+ *     type: apiKey
+ *     name: Authorization
+ *     in: header
+ *   security:
+ *     - BearerAuth: []
+ *   parameters:
+ *    - name: id
+ *      in: path
+ *      description: appointment's id
+ *      required: true
+ *      type: uuid
+ *   responses:
+ *    200:
+ *     description: an appointment which match ID
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Appointment'
+ *    404:
+ *     description: No appointment match the ID
+ *    500:
+ *     description: Can't find appointment
+ *      
+ *  put:
+ *   summary: Update an appointment
+ *   tags: [Appointments]
+ *   securityDefinitions:
+ *     BearerAuth:
+ *      type: apiKey
+ *      name: Authorization
+ *      in: header
+ *   security:
+ *     - BearerAuth: []
+ *   parameters:
+ *    - name: id
+ *      in: path
+ *      description: appointment's id
+ *      required: true
+ *      type: uuid
+ *   requestBody:
+ *     description: appointment data which want to update
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: '#/components/schemas/Appointment'
+ *   responses:
+ *    200:
+ *     description: an appointment which match ID
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Appointment'
+ *    400:
+ *     description: No dentist's available datetime with the id of dentistId
+ *    401:
+ *     description: User is not authorized to update
+ *    404:
+ *     description: No dentist match dentistID
+ *    500:
+ *     description: Cannot update Appointment
+ *  delete:
+ *   summary: delete an appointment
+ *   tags: [Appointments]
+ *   securityDefinitions:
+ *     BearerAuth:
+ *      type: apiKey
+ *      name: Authorization
+ *      in: header
+ *   security:
+ *     - BearerAuth: []
+ *   parameters:
+ *    - name: id
+ *      in: path
+ *      description: appointment's id
+ *      required: true
+ *      type: uuid
+ *   responses:
+ *    200:
+ *     description: Success delete an appointment
+ *    400:
+ *     description: Can't find appointment to delete or something went wrong
+ *    401:
+ *     description: User is not authorized to delete this appointment
+ *    404:
+ *     description: No appointment match the ID
+ *    500:
+ *     description: Cannot delete Appointment
+ */ 
+
+/**
+ * @swagger
+ * /dentist/{dentistId}/appointment:
+ *  post: 
+ *   summary: create an appointment
+ *   tags: [Appointments]
+ *   security:
+ *     - BearerAuth: []
+ *   parameters:
+ *    - name: dentistId
+ *      in: path
+ *      description: dentist's id
+ *      required: true
+ *      type: uuid
+ *   requestBody:
+ *     description: appointment data which want to create
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: '#/components/schemas/Appointment'
+ *   responses:
+ *    200:
+ *     description: success to create an appointment
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: '#/components/schemas/Appointment'
+ *    400:
+ *     description: No dentist's available datetime with the id of dentistId or user has already made 1 appointment
+ *    404:
+ *     description: No dentist match the dentistID
+ *    500:
+ *     description: Cannot create Appointment
+ *   securityDefinitions:
+ *     BearerAuth:
+ *      type: apiKey
+ *      name: Authorization
+ *      in: header
+ */ 
